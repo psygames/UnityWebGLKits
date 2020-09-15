f9eb22e3cecd9b0c2b49390d13121b601175ddbe
yinlong <yinlong@coolfishgames.com>
2020-09-15 10:49:00 +0800

    show image

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -9,6 +9,10 @@ public class Test : MonoBehaviour
     int width = 0;
     int height = 0;
     string jsCode = "alert(1+2)";
+    string uploadFileName = "";
+    byte[] uploadBytes = null;
+    Texture2D uploadTexture = null;
+    Vector2 scroll = Vector2.zero;
 
     private void Awake()
     {
@@ -18,6 +22,7 @@ public class Test : MonoBehaviour
 
     private void OnGUI()
     {
+        scroll = GUILayout.BeginScrollView(scroll);
         GUILayout.BeginVertical("box");
         GUILayout.Label("Base:");
         if (GUILayout.Button("console.log"))
@@ -32,16 +37,30 @@ public class Test : MonoBehaviour
         GUILayout.EndVertical();
 
         GUILayout.BeginVertical("box");
-        if (GUILayout.Button("UploadFile"))
+        if (GUILayout.Button("Upload File"))
         {
+            uploadFileName = "";
+            uploadBytes = null;
+            uploadTexture = null;
             WebGLKit.UploadFile((filename, bytes) =>
             {
                 WebGLKit.Log("file name: " + filename);
                 WebGLKit.Log("file size: " + bytes.Length);
 
+                uploadFileName = filename;
+                uploadBytes = bytes;
                 // Do something else
             });
         }
+        GUILayout.Label("file:" + uploadFileName);
+        if (uploadFileName.EndsWith(".png") || uploadFileName.EndsWith(".jpg")
+            || uploadFileName.EndsWith(".jpeg"))
+        {
+            if (uploadTexture == null)
+                uploadTexture = new Texture2D(200, 100);
+            uploadTexture.LoadImage(uploadBytes);
+            GUILayout.Box(uploadTexture, GUILayout.Width(200), GUILayout.Height(100));
+        };
         GUILayout.EndVertical();
 
         GUILayout.BeginVertical("box");
@@ -66,5 +85,6 @@ public class Test : MonoBehaviour
             WebGLKit.Log(result);
         }
         GUILayout.EndVertical();
+        GUILayout.EndScrollView();
     }
 }
